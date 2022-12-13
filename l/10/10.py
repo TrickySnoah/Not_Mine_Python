c047b1e0aad7acbdd85d65f73c95928cbf6baecd
NoahJack143 <noah.jack.709@gmail.com>
2022-12-13 14:47:03 -0600

    Update 10.py

--- l/10/10.py
+++ l/10/10.py
@@ -1,1 +1,20 @@
-#THIS HAS NOTHING YET. THIS WILL BE FINISHED TOMORROW
+import pickle
+import random
+def menu():
+   try:
+       inve=open("INV.dat","rb")
+       inv=pickle.load(inve)
+       inve.close()
+    except:
+        inve=open("INV.dat","wb")
+        inve.close()
+        print("the inventory was empty")
+    if len(inv)>0:
+        print("menu\n1 to go to the inv\n2 to go to the store")
+        opt=input("where do you want to go?")
+        if opt=="1":
+            inventory()
+        elif opt=="2":
+            store()
+def inventory():
+    print('menu\n1 to show the inv\n2to add stuff to the inv\n3 to save the inv\n4 to leave
